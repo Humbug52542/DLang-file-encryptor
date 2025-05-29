fc5532992690a7938e697865d548d010bc9a9618
Humbug!! <xmansussy72@gmail.com>
2025-05-29 13:32:28 +0000

    Update Encrypter.d

--- Encrypter.d
+++ Encrypter.d
@@ -13,3 +13,18 @@
 //
 // © 2025 Humbug52542. Licensed under the MIT License.
 // ======================================================================
+import core.stdc.stdlib;
+import core.simd;
+import core.thread;
+import std.base64;
+import std.file;
+import std.path;
+import std.random;
+import std.stdio;
+import std.parallelism;
+import std.process;
+import std.conv;
+import core.sys.windows.windows;
+import core.sys.windows.psapi;
+import core.sys.windows.ntdll;
+// Thats all for now... 5/29 
