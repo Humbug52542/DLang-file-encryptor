3917876abbb6fc98fcab4712f2856346cfa561fa
Humbug!! <195818135+Humbug52542@users.noreply.github.com>
2025-06-11 15:39:50 -0400

    Update Encrypter.d

--- Encrypter.d
+++ Encrypter.d
@@ -1,18 +1,4 @@
-// ======================================================================
-// Use this software responsibly and ethically at all times.
-// Unauthorized or malicious use of this program is strictly prohibited.
-// The author, Humbug52542, disclaims any liability for damages or losses 
-// arising from misuse, improper modification, or distribution of this software.
-// 
-// By using this software, you agree to abide by all applicable laws and 
-// respect the rights and privacy of others.
-// 
-// For questions, ethical concerns, or to report vulnerabilities, please contact:
-//   Email: xmansussy@yahoo.com
-//   Discord: sussysfc_52542
-//
-// © 2025 Humbug52542. Licensed under the MIT License.
-// ======================================================================
+
 import core.stdc.stdlib;
 import core.simd;
 import core.thread;
